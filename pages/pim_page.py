b25e60d400870dad30f58b9b941aa37c9f4e9a4c
hasanali672 <hassan.ali@fetchsky.com>
2025-12-18 21:15:16 +0500

    added testcasos of search employee in PIM screen

--- pages/pim_page.py
+++ pages/pim_page.py
@@ -10,7 +10,11 @@ class PIM(BasePage):
       self.add_employee_button = (By.XPATH, "//button[normalize-space()='Add']")
       self.reset_button = (By.XPATH, "//button[normalize-space()='Reset']")
       self.search_button = (By.XPATH, "//button[normalize-space()='Search']")
-
+      # Employee Information section
+      self.employee_name_field = (By.XPATH, "//div[@class='oxd-autocomplete-text-input oxd-autocomplete-text-input--focus']")
+      self.no_records_found_for_listbox = (By.XPATH, "//div[@role='option']")
+      self.no_records_found_for_table = (By.XPATH, "//span[normalize-space()='No Records Found']")
+      self.message_records_found = (By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/span")
 
     def get_pim_page_heading(self):
       if self.is_displayed(self.pim_page_heading):
@@ -35,6 +39,44 @@ class PIM(BasePage):
     def click_on_reset_button(self):
       if self.is_displayed(self.reset_button):
         self.click(self.reset_button)
+
+    def enter_value_in_employee_name_field(self):
+      if self.is_displayed(self.employee_name_field):
+        self.click(self.employee_name_field)
+        self.send_keys(self.employee_name_field, "Hassan Ali Khan")
+
+    def verify_searched_record_not_listed(self):
+      actual_message = "No Records Found"
+      if self.is_displayed(self.no_records_found_for_listbox):
+        expected_message = self.get_text(self.no_records_found_for_listbox)
+        if expected_message == actual_message:
+          return True
+        else:
+          return False
+      else:
+        return "User has searched in the list"
+
+    def verify_searched_record_not_in_table(self):
+      actual_message = "No Records Found"
+      if self.is_displayed(self.no_records_found_for_table):
+        expected_message = self.get_text(self.no_records_found_for_table)
+        if expected_message == actual_message:
+          return True
+        else:
+          return False
+      else:
+        return "Searched record not available in the table"
+
+    def verify_records_listed_in_table_on_reset(self):
+      expected_message = "No Records Found"
+      if self.is_displayed(self.message_records_found):
+        actual_message = self.get_text(self.message_records_found)
+        if expected_message != actual_message:
+          return True
+        else:
+          return False
+      else:
+        return "On reset, records not listed in the table"
 
     def add_employee_successfully(self):
       if self.is_displayed(self.employee_information_heading):
